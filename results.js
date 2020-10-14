937efda51d4897d4aa5fa68f4c9d3dab3f287192
Katelinn Jones <katiejonesyo@gmail.com>
2020-10-14 10:02:15 -0700

    styling changes and chart additions

--- results.js
+++ results.js
@@ -16,5 +16,210 @@ return bagItem.encountered;
 });
 
 
-
 //Chart info goes below 
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var myChart = newChart(ctx, {
+
+    type: 'bar',
+
+    data: {
+
+        labels: pokemonLabels,
+
+        datasets: [{
+
+            label: '# of Pokemon Captured',
+
+            data: pokemonCaught,
+
+            backgroundColor: [
+
+                'rgba(255, 99, 132, 0.7)',
+
+                'rgba(54, 162, 235, 0.7)',
+
+                'rgba(255, 206, 86, 0.7)',
+
+                'rgba(75, 192, 192, 0.7)',
+
+                'rgba(153, 102, 255, 0.7)',
+
+                'rgba(255, 159, 64, 0.7)',
+
+                'rgba(255, 99, 132, 0.7)',
+
+                'rgba(54, 162, 235, 0.7)',
+
+                'rgba(255, 206, 86, 0.7)',
+
+                'rgba(75, 192, 192, 0.7)',
+
+                'rgba(153, 102, 255, 0.7)',
+
+                'rgba(255, 159, 64, 0.7)',
+
+                'rgba(255, 99, 132, 0.7)'
+
+            ],
+
+            borderColor: [
+
+                'rgba(255, 99, 132, 1)',
+
+                'rgba(54, 162, 235, 1)',
+
+                'rgba(255, 206, 86, 1)',
+
+                'rgba(75, 192, 192, 1)',
+
+                'rgba(153, 102, 255, 1)',
+
+                'rgba(255, 159, 64, 1)',
+
+                'rgba(255, 99, 132, 1)',
+
+                'rgba(54, 162, 235, 1)',
+
+                'rgba(255, 206, 86, 1)',
+
+                'rgba(75, 192, 192, 1)',
+
+                'rgba(153, 102, 255, 1)',
+
+                'rgba(255, 159, 64, 1)',
+
+                'rgba(255, 99, 132, 1)'
+
+            ],
+
+            borderWidth: 3
+
+        }]
+
+    },
+
+    options: {
+
+        scales: {
+
+            yAxes: [{
+
+                ticks: {
+
+                    beginAtZero: true
+
+                }
+
+            }]
+
+        }
+
+    }
+
+});
+
+//my other chart
+
+var ctx = document.getElementById('myOtherChart').getContext('2d');
+
+var myOtherChart = new Chart(ctx, {
+
+    type: 'bar',
+
+    data: {
+
+        labels: pokemonLabels,
+
+        datasets: [{
+
+            label: '# of Pokemon Encountered',
+
+            data: pokemonEncountered,
+
+            backgroundColor: [
+
+                'rgba(255, 99, 132, 0.7)',
+
+                'rgba(54, 162, 235, 0.7)',
+
+                'rgba(255, 206, 86, 0.7)',
+
+                'rgba(75, 192, 192, 0.7)',
+
+                'rgba(153, 102, 255, 0.7)',
+
+                'rgba(255, 159, 64, 0.7)',
+
+                'rgba(255, 99, 132, 0.7)',
+
+                'rgba(54, 162, 235, 0.7)',
+
+                'rgba(255, 206, 86, 0.7)',
+
+                'rgba(75, 192, 192, 0.7)',
+
+                'rgba(153, 102, 255, 0.7)',
+
+                'rgba(255, 159, 64, 0.7)',
+
+                'rgba(255, 99, 132, 0.7)'
+
+            ],
+
+            borderColor: [
+
+                'rgba(255, 99, 132, 1)',
+
+                'rgba(54, 162, 235, 1)',
+
+                'rgba(255, 206, 86, 1)',
+
+                'rgba(75, 192, 192, 1)',
+
+                'rgba(153, 102, 255, 1)',
+
+                'rgba(255, 159, 64, 1)',
+
+                'rgba(255, 99, 132, 1)',
+
+                'rgba(54, 162, 235, 1)',
+
+                'rgba(255, 206, 86, 1)',
+
+                'rgba(75, 192, 192, 1)',
+
+                'rgba(153, 102, 255, 1)',
+
+                'rgba(255, 159, 64, 1)',
+
+                'rgba(255, 99, 132, 1)'
+
+            ],
+
+            borderWidth: 3
+
+        }]
+
+    },
+
+    options: {
+
+        scales: {
+
+            yAxes: [{
+
+                ticks: {
+
+                    beginAtZero: true
+
+                }
+
+            }]
+
+        }
+
+    }
+
+});
